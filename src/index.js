11393f2cfa521e9cd56b51234de809639e216738
vitalves <vitalvesilva@gmail.com>
2019-09-17 20:21:30 -0300

    confis iniciais

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// arquivo de entrada da aplicacao (tudo parte dele)
+const soma = (a, b) => a + b;
+alert(soma(1,4));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+// para alterar o caminho de entry: 'src/index.js' para: path.resolve(__dirname, 'src', 'index.js'); 
+const path = require('path');
+
+module.exports = {
+  // arquivo de entrada
+  // entry: 'src/index.js' // alterado para compatibilidade com SOs
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  // onde ele vai jogar o bundle (codigo transpilado):
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    // onde sera encontrado o index.html
+    contentBase: path.resolve(__dirname, 'public'), //
+  },
+  // config: regras para tipos de arquivos
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // encontre arquivo que termine em ".js"
+        exclude: /node_modules/, // ignora tudo da pasta informada 
+        use: {
+          loader: 'babel-loader', // babel-loader -D 
+        }
+      }
+    ]
+  }
+}
