0ceb04bd4e62b4ad1be1afe9c6ca23574ab03a39
vitalves <vitalvesilva@gmail.com>
2019-09-18 11:39:31 -0300

    Criando componente raiz

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// sempre que a sintax JSX for utilizada o React precsa ser importado:
+
+function App() {
+  return <h1>Hello World</h1>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 // arquivo de entrada da aplicacao (tudo parte dele)
-const soma = (a, b) => a + b;
-alert(soma(1,4));
+// const soma = (a, b) => a + b;
+// alert(soma(1,4));
+
+import React from 'react';
+import { render } from 'react-dom';
+
+// App
+import App from './App';
+
+// render(<h1>Hello World</h1>, document.getElementById('app'));
+
+render(<App />, document.getElementById('app'));
