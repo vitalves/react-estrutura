785c9762f8e85da695ea8df2f2fb3ed723dfcd1d
vitalves <vitalvesilva@gmail.com>
2019-09-19 19:19:03 -0300

    Propriedades do React

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,5 +1,6 @@
 // class component
 import React, { Component } from 'react';
+import TechItem from './TechItem';
 
 class TechList extends Component {
   // para usar status sem um constructor:
@@ -38,16 +39,19 @@ class TechList extends Component {
         <h1> {/* this.state.newTech */ } </h1>
         <ul>
           { this.state.techs.map(tech =>( 
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button"> Remover </button>
-            </li> 
+            <TechItem
+              key={tech}
+              tech={tech}
+              // icon={<Icon />}
+              onDelete={() => this.handleDelete(tech)}
+            />
           )) }
         </ul>
         <input 
           type="text" 
           onChange={this.handleInputChange} 
           value={this.state.newTech} 
+          
         />
         <button type="submit">Enviar</button>
       </form>
