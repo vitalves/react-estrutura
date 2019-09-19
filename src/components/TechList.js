0bed844ce2c19800c61d9a1930d9e30455f01ec2
vitalves <vitalvesilva@gmail.com>
2019-09-19 18:27:54 -0300

    Estado & Imutabilidade

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,23 +5,43 @@ class TechList extends Component {
   // para usar status sem um constructor:
   // yarn add @babel/plugin-proposal-class-properties -D
   state = {
+    newTech: '',
     techs: [
       'Node.js',
       'React JS',
       'React Netive',
-    ]
+    ],
   };
 
+  handleInputChange = e => {
+    // console.log(e.target.value);
+    this.setState({ newTech: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // console.log(this.state.newTech);
+    this.setState({ 
+      techs: [...this.state.techs, this.state.newTech ],
+      newTech: '',
+    });
+  }
 
   render() {
-    console.log(this.state);
-
+    // console.log(this.state);
     return(
-      <ul>
-        <li>Node.js</li>
-        <li>React JS</li>
-        <li>React Netive</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <h1> {/* this.state.newTech */ } </h1>
+        <ul>
+          { this.state.techs.map(tech => <li key={tech}>{tech}</li>) }
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech} 
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
